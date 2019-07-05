70669a902c9324496c06bd888b3f0bdc1ba8ac7c
okarabulut07 <o.karabulut07@gmail.com>
2019-07-05 12:04:38 +0300

    landMarkBook

---
+++ app/src/main/java/com/example/landmarkbook/Globals.java
@@ -0,0 +1,29 @@
+package com.example.landmarkbook;
+
+import android.graphics.Bitmap;
+
+public class Globals {
+
+    private static Globals instance;
+
+    private Bitmap chosenImage;
+
+   private Globals(){
+
+   }
+
+   public void setData(Bitmap chosenImage){
+       this.chosenImage = chosenImage;
+   }
+
+   public Bitmap getData(){
+       return this.chosenImage;
+   }
+
+   public static Globals getInstance(){
+       if(instance == null){
+           instance = new Globals();
+       }
+       return instance;
+   }
+}
